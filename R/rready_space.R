3f23992991820fbf7730b59914bf560d89fb2d38
Matthew Hamilton <mahamilton@orygen.org.au>
2019-05-17 16:58:47 +1000

    version

--- R/rready_space.R
+++ R/rready_space.R
@@ -1,23 +1,10 @@
-#' ready.space: Package of functions for modelling in space and time with the readfyforwhatsnext suite of resilience packages.
+#' ready.space: Create Readyforwhatsnext Spatio-temporal Objects.
 #'
 #' ready.space is a package of functions that creates the spatial and temporal environment for the readyforwhatsnext simulation toolkit.
 #'
-#' It has three main goals:
-#' \itemize{
-#' \item Goal 1.
-#' \item Goal 2
-#' \item Goal 3.
-#' }
 #' To learn more about ready.space, start with the vignettes:
 #' `browseVignettes(package = "ready.space")`
 #' @section First section:
-#' \describe{
-#' \item{firstitem}
-#' }
-#' @section Second section:
 #' Some text.
-#' \describe{
-#' \item{moretext}
-#' }
 #'
 "_PACKAGE"
