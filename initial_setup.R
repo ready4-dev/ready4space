da894ed9bfc0e59b76116515e6f2ca468f5726c9
Matthew Hamilton <mahamilton@orygen.org.au>
2019-06-15 15:15:35 +1000

    version

--- initial_setup.R
+++ initial_setup.R
@@ -1,7 +1,7 @@
 usethis::create_package(getwd())
 #
 usethis::use_dev_package("ready4agents")
-usethis::use_dev_package("ready4epi")
+usethis::use_dev_package("ready4sd")
 usethis::use_dev_package("ready4s4")
 usethis::use_dev_package("ready4utils")
 usethis::use_package("dplyr")
