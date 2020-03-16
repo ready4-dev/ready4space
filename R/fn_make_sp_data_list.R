ca8d6918c013767072043cfe3155930e0d4097f8
Matthew Hamilton <mahamilton@orygen.org.au>
2020-03-16 12:32:07 +1100

    v9190

--- R/fn_make_sp_data_list.R
+++ R/fn_make_sp_data_list.R
@@ -263,13 +263,16 @@ get_data_year_chr <- function(data_ymdhms){
 #'  \code{\link[lubridate]{period}}
 #' @rdname get_model_end_ymdhs
 #' @export
-#' @importFrom lubridate years weeks days
+#' @importFrom lubridate years weeks days hours minutes seconds
 get_model_end_ymdhs <- function(input_ls){
   input_ls$model_start_ymdhms +
     lubridate::years(input_ls$simulation_steps_ymwd[1]) * input_ls$nbr_steps_start_to_end +
     months(input_ls$simulation_steps_ymwd[2]) * input_ls$nbr_steps_start_to_end +
     lubridate::weeks(input_ls$simulation_steps_ymwd[3]) * input_ls$nbr_steps_start_to_end +
-    lubridate::days(input_ls$simulation_steps_ymwd[4]) * input_ls$nbr_steps_start_to_end
+    lubridate::days(input_ls$simulation_steps_ymwd[4]) * input_ls$nbr_steps_start_to_end +
+    lubridate::hours(input_ls$simulation_steps_ymwd[5]) * input_ls$nbr_steps_start_to_end +
+    lubridate::minutes(input_ls$simulation_steps_ymwd[6]) * input_ls$nbr_steps_start_to_end +
+    lubridate::seconds(input_ls$simulation_steps_ymwd[7]) * input_ls$nbr_steps_start_to_end
 }
 
 #' @title add_dynamic_sp_vars_to_sf
