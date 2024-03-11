0a60eb953403008af187f47b81f44a925c0132d1
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-03-11 16:33:31 +1100

    another bug fix

--- R/mthd_author.R
+++ R/mthd_author.R
@@ -111,10 +111,10 @@ methods::setMethod("author", "VicinityLocalProcessed", function (x, crs_nbr_dbl,
     vicinity_raw_r3 <- x@a_VicinityLookup@vicinity_raw_r3
     ready4use::assert_single_row_tb(vicinity_raw_r3)
     imports_ls <- ingest(x = vicinity_raw_r3, imports_chr = x@imports_chr, 
-        data_type_1L_chr = vicinity_raw_r3$data_type, raw_fls_dir_1L_chr = x@raw_fls_dir_1L_chr, 
+        data_type_1L_chr = vicinity_raw_r3$data_type_chr, raw_fls_dir_1L_chr = x@raw_fls_dir_1L_chr, 
         processed_fls_dir_1L_chr = x@processed_fls_dir_1L_chr, 
         write_1L_lgl = x@write_1L_lgl) %>% stats::setNames(x@imports_chr)
-    if (vicinity_raw_r3$data_type == "Geometry") {
+    if (vicinity_raw_r3$data_type_chr == "Geometry") {
         path_to_seed_sf_1L_chr <- make_paths_to_fls_for_ingest(processed_fls_dir_1L_chr = x@processed_fls_dir_1L_chr, 
             name_chr = names(imports_ls)[1], data_type_chr = "Geometry")
     }
